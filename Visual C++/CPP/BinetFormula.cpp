92fef420921c9a23cbfeb7fe28c2e165b49e56ec
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-11-28 21:12:17 +0530

    Add files via upload

---
+++ Visual C++/CPP/BinetFormula.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    ll n,ans,s1,s2;
+    cin>>n;
+
+    s1 = 1 + sqrt(5);
+    s2 = 1 - sqrt(5);
+
+    ans = (pow(s1,n) - pow(s2,n))/(pow(2,n) * sqrt(5));
+
+    cout<<ans;
+ 
+return 0;
+   
+}
