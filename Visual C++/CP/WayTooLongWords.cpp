d230138dba3ebff168b4dd4c25665d1ac13f00e2
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-11-28 21:11:32 +0530

    Add files via upload

---
+++ Visual C++/CP/WayTooLongWords.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    ll n,m;
+    cin>>n;
+ 
+    while(n--){
+        string s;
+        cin>>s;
+        m = s.length();
+        if (m <= 10) cout<<s<<endl;
+        else{
+        cout<<s[0]<<m-2<<s[m-1]<<endl;
+        }
+    }
+ 
+return 0;
+   
+}
