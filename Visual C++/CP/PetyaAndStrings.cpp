14a6656b04b59d0353da77b8480beb62d133f8b9
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-11-29 16:45:39 +0530

    Add files via upload

---
+++ Visual C++/CP/PetyaAndStrings.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    string a,b; 
+    cin>>a>>b;
+ 
+    loop(i,0,a.size()){
+        if (a[i] < 92) {
+            a[i] += 32;
+        }
+        if (b[i] < 92) {
+            b[i] += 32;
+        }
+    }
+    if (a < b) {
+        cout << -1;
+    } else if (a > b) {
+        cout << 1;
+    } else if (a == b) {
+        cout << 0;
+    }
+ 
+ 
+return 0;
+   
+}
