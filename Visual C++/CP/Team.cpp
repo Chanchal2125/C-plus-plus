90d806ecf13db7598766bc29db20ec2dd6dc642b
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-11-29 16:43:46 +0530

    Add files via upload

---
+++ Visual C++/CP/Team.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    ll n,count = 0;
+    cin>>n;
+    vector <ll> dp(3);
+    while(n--){
+        ll sum = 0;
+        loop(i,0,2){
+            cin>>dp[i];
+            sum += dp[i];
+        }
+        if (sum >= 2) count += 1;
+    }
+    cout<<count;
+ 
+return 0;
+   
+}
