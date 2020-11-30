e6d90e18050263b86c1cb9f3e926d07786e3c484
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-11-30 21:17:20 +0530

    Add files via upload

---
+++ Visual C++/CP/Tram.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ /*
+    4
+    0 3
+    2 5          
+    4 2
+    4 0
+
+ */
+ 
+int main() 
+{
+    ll n,sum = INT_MIN,m = 0;
+    cin>>n;
+ 
+    while(n--){
+        ll a,b;
+        cin>>a>>b;
+        m = m - a;
+        m = m + b;
+        sum = max(sum,m);
+    }
+
+    cout<<sum;
+return 0;
+   
+}
