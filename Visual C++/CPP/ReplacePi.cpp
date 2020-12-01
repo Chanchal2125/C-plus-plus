37018daf525100186a232056ca92562bc9dd4076
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-01 23:10:20 +0530

    Add files via upload

---
+++ Visual C++/CPP/ReplacePi.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ void replace(string s){
+     if (s.length() == 0) return;  //Base Case
+     if (s[0] == 'p' && s[1] == 'i'){
+         cout<<"3.14";
+         replace(s.substr(2));
+     }
+     else{
+         cout<<s[0];
+         replace(s.substr(1));
+     }
+ }
+
+int main() 
+{
+    string s;
+    cin>>s;
+ 
+    replace(s);
+ 
+return 0;
+   
+}
