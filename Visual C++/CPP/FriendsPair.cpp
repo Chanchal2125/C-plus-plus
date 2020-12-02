e4578258ed2af4e462ba4e781cf54068d4ff10b8
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-02 23:09:40 +0530

    Add files via upload

---
+++ Visual C++/CPP/FriendsPair.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ll frnd(int n){
+
+    if (n == 0 || n == 1 || n == 2){
+        return n;
+    }
+
+    return frnd(n-1) + frnd(n-2)*(n-1);
+}
+
+int main() 
+{
+    ll n;
+    cin>>n;
+ 
+    cout<<frnd(n);
+ 
+return 0;
+   
+}
