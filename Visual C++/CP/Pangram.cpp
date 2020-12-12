ec67e4a28ff30eba14c9c0995440f1688f2b0643
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-12 23:04:33 +0530

    Add files via upload

---
+++ Visual C++/CP/Pangram.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    int n;
+     string s;
+     char j='A';
+     cin>>n;
+     cin>>s;
+     for(int k=0;k<n;k++){
+     s[k]=toupper(s[k]);
+     }
+
+if(n>=26){
+for(int i=0;i<26;i++){
+if(s.find(j)!=-1){
+ j++;
+}
+else{
+cout<<"NO";
+return 0;
+}
+}
+cout<<"YES";
+}
+else{
+cout<<"NO";
+    }
+return 0;
+   
+}
