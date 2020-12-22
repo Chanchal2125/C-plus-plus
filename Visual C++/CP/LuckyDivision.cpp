8383fb3e358c41a6bf64965d2d00cbc475310bbd
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-22 13:59:58 +0530

    Add files via upload

---
+++ Visual C++/CP/LuckyDivision.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    int n;
+cin>>n;
+bool flag=0;
+int arr[12]={4,7,47,74,44,444,447,474,477,777,774,744};
+ for(int i=0;i<12;i++){
+	 if(n%arr[i]==0){
+		 flag=true;
+	 }
+ }
+ if(flag)
+cout<<"YES";
+else
+	cout<<"NO";
+ 
+return 0;
+   
+}
