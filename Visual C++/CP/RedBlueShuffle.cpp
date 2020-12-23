26b60a9f0d705269a193ac5837b5c0ba524d9635
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-23 19:45:27 +0530

    Add files via upload

---
+++ Visual C++/CP/RedBlueShuffle.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    ll t;
+    cin>>t;
+    
+    while(t--){
+        ll n;
+        cin>>n;
+        string a,b;
+        cin>>a>>b;
+        ll r = 0, d = 0;
+        loop(i,0,n-1){
+            if (a[i] > b[i]) {
+                r += 1;
+            }
+            else if (a[i] < b[i]) {
+                d += 1;
+            }
+        }
+        if (r == d) {
+            cout<<"EQUAL"<<endl;
+        }
+        else if (r < d) {
+            cout<<"BLUE"<<endl;
+        }
+        else{
+            cout<<"RED"<<endl;
+        }
+    }
+ 
+return 0;
+   
+}
