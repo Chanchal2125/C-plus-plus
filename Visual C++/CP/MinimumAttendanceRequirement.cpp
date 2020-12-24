6bceac3a0c2885f4046707081cc19684c2404fcc
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-24 21:42:06 +0530

    Add files via upload

---
+++ Visual C++/CP/MinimumAttendanceRequirement.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include<stdlib.h>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    ll t;
+    cin>>t;
+ 
+    while(t--){
+        ll n,sum = 0;
+        cin>>n;
+        string s;
+        cin>>s;
+
+        ll m = 120 - n;
+        loop(i,0,n-1){
+            if (s[i] == '1') sum += 1;
+        }
+        if(sum + m >= 90){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+ 
+ 
+return 0;
+   
+}
