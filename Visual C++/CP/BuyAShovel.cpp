2b864312d873b574a8d2add704b6b4894459d5bc
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-25 16:15:45 +0530

    Add files via upload

---
+++ Visual C++/CP/BuyAShovel.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    ll k,n,c = 1;
+    cin>>k>>n; 
+
+    loop(i,1,10e9){
+        ll x = k * i;
+        if ((x-n) % 10 == 0 || x % 10 == 0){
+            cout<<c;
+            return 0;
+        }
+        else{
+            c += 1;
+        }
+    }
+
+return 0;
+   
+}
