7f570ed7a4d151f8b5edb6b059aec7f164b5a3ef
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-27 22:01:40 +0530

    Add files via upload

---
+++ Visual C++/CP/OneZeroSwaps.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        char a[n];
+        char b[n];
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        for(int i=0; i<n; i++){
+            cin>>b[i];
+        }
+        int cnt1 = 0, cnt2  =0;
+        bool ans = true;
+        for(int i=0; i<n; i++){
+            if(a[i] == '1') cnt1++;
+            if(b[i] == '1') cnt2++;
+            if(cnt2> cnt1){
+                ans = false;
+                 break;
+            }
+        }
+        if(cnt1 != cnt2) ans = false;
+        if(ans) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+return 0;
+   
+}
